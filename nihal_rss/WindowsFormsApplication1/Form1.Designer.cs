3d7769708872a9b079033809081512698d4499ef
Nihal Lodhi <58701929+nihallodhi@users.noreply.github.com>
2022-01-02 10:19:35 +0530

    Add files via upload
    Adding code changes

---
+++ nihal_rss/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 287);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1523, 539);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Google News",
+            "The Times of India",
+            "The Times of India(World)",
+            "The Hindu",
+            "ESPNCRICINFO(Scores)",
+            "ESPNCRICINFO(News)",
+            "DAILYMAIL",
+            "MoneyControl",
+            "IndiaToday"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 287);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.loadlistview);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(184, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1327, 284);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.loadbrowser);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 826);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Latest News - Nihal LODHI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ nihal_rss/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SyndicationFeed feed;
+        String []links = new String[100];
+
+        
+        
+       // char[] ch = 'url';
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void loadlistview(object sender, EventArgs e)
+        {
+            String option = listBox1.SelectedItem.ToString();
+            String url = "";
+
+            if(option.Equals("Google News"))
+            {
+                listBox2.Items.Clear();
+                url = "https://news.google.co.in/news?cf=all&hl=en&pz=1&ned=in&output=rss";
+                parseurl(url);
+
+            }
+            else if(option.Equals("The Times of India"))
+            {
+                listBox2.Items.Clear();
+                url = "http://timesofindia.indiatimes.com/rssfeedstopstories.cms";
+                parseurl(url);
+            }
+            else if (option.Equals("The Times of India(World)"))
+            {
+                listBox2.Items.Clear();
+                url = "http://timesofindia.indiatimes.com/rssfeeds/296589292.cms";
+                parseurl(url);
+            }
+            else if (option.Equals("The Hindu"))
+            {
+                listBox2.Items.Clear();
+                url = "http://www.thehindubusinessline.com/?service=rss";
+                parseurl(url);
+            }        
+            else if (option.Equals("ESPNCRICINFO(Scores)"))
+            {
+                listBox2.Items.Clear();
+                url = "http://static.cricinfo.com/rss/livescores.xml";
+                parseurl(url);            
+            }
+            else if (option.Equals("DAILYMAIL"))
+            {
+                listBox2.Items.Clear();
+                url = "http://www.dailymail.co.uk/articles.rss";
+                parseurl(url);
+            }
+            else if(option.Equals("ESPNCRICINFO(News)"))
+            {
+                listBox2.Items.Clear();
+                url = "http://www.espncricinfo.com/rss/content/story/feeds/0.xml";
+                parseurl(url);            
+            }
+            else if(option.Equals("MoneyControl"))
+            {
+                listBox2.Items.Clear();
+                url = "http://www.moneycontrol.com/rss/MCtopnews.xml";
+                parseurl(url);
+            }
+            else if (option.Equals("IndiaToday"))
+            {
+                listBox2.Items.Clear();
+                url = "http://indiatoday.intoday.in/rss/homepage-topstories.jsp";
+                parseurl(url);
+            }
+        
+        }
+
+        public void parseurl(String url)
+        {
+            using (XmlReader reader = XmlReader.Create(url))
+            {
+                feed = SyndicationFeed.Load(reader);
+                int i = 0;
+                foreach(SyndicationItem item in feed.Items)
+                {
+                   
+                    listBox2.Items.Add(item.Title.Text);
+                    links[i++] = item.Links[0].Uri.ToString();
+
+                    //listBox3.Items.Add(item.Categories.ToString());
+                    //link = item.Links[0].Uri.ToString();
+                    //listBox3.Items.Add(item.Links[0].Uri);
+
+                    if (i == 99)
+                        break;
+                }
+
+            }
+
+        }
+
+        private void loadbrowser(object sender, EventArgs e)
+        {
+            int ind = listBox2.SelectedIndex;
+
+            //listBox3.Items.Add(links[ind]);
+            //parsestring(ind); 
+            
+            webBrowser1.Navigate(links[ind]);
+        }
+
+        private void parsestring(int i)
+        {
+            String web = links[i];
+
+            
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            //int width = sender
+            //this.AutoSize = true;
+            //this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+    }
+}
